53658f4e5c24673de05785b3f26c4e76806e58c2
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-08-10 19:30:23 -0300

    Adding only light theme, and removing the action bar

--- app/src/main/java/dev/gustavodahora/imagefree/MainActivity.java
+++ app/src/main/java/dev/gustavodahora/imagefree/MainActivity.java
@@ -1,6 +1,7 @@
 package dev.gustavodahora.imagefree;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Only Light Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
