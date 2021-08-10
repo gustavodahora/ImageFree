ffa8542f9608431c4c5952632a939d77188bef9d
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-08-10 20:15:18 -0300

    Adding a new recycler view on activity main, and creating the shape for images recyler on layout

--- app/src/main/java/dev/gustavodahora/imagefree/MainActivity.java
+++ app/src/main/java/dev/gustavodahora/imagefree/MainActivity.java
@@ -2,10 +2,13 @@ package dev.gustavodahora.imagefree;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Only Light Mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
